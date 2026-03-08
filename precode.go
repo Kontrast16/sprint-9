c01aa90a61085a52aa9cc738ddb6f7a9a70628cc
kontrast <100769393+Kontrast16@users.noreply.github.com>
2026-03-08 22:18:28 +0300

    Add files via upload

---
+++ precode.go
@@ -0,0 +1,115 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"sync"
+	"time"
+)
+
+// Generator генерирует последовательность чисел 1,2,3 и т.д. и
+// отправляет их в канал ch. При этом после записи в канал для каждого числа
+// вызывается функция fn. Она служит для подсчёта количества и суммы
+// сгенерированных чисел.
+func Generator(ctx context.Context, ch chan<- int64, fn func(int64)) {
+
+    var i int64 = 1
+    for { 
+        select {
+        case <-ctx.Done(): 
+
+            close(ch) 
+            return
+
+        case ch <- i: 
+            fn(i) 
+            i++
+        }
+    }
+}
+
+// Worker читает число из канала in и пишет его в канал out.
+func Worker(in <-chan int64, out chan<- int64) {
+
+	defer close(out)
+    for v := range in {
+		out <- v 
+	}
+
+}
+
+func main() {
+	chIn := make(chan int64)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 2 * time.Second)
+	defer cancel()
+
+	var inputSum int64   // сумма сгенерированных чисел
+	var inputCount int64 // количество сгенерированных чисел
+
+	go Generator(ctx, chIn, func(i int64) {
+		inputSum += i
+		inputCount++
+	})
+
+	const NumOut = 5 // количество обрабатывающих горутин и каналов
+	// outs — слайс каналов, куда будут записываться числа из chIn
+	outs := make([]chan int64, NumOut)
+	for i := 0; i < NumOut; i++ {
+		outs[i] = make(chan int64)
+		go Worker(chIn, outs[i])
+	}
+
+	// amounts — слайс, в который собирается статистика по горутинам
+	amounts := make([]int64, NumOut)
+	// chOut — канал, в который будут отправляться числа из горутин `outs[i]`
+	chOut := make(chan int64, NumOut)
+
+	var wg sync.WaitGroup
+
+	for i := 0; i < NumOut; i++ {
+		wg.Add(1)
+
+		go func(index int, c <- chan int64) {
+			defer wg.Done()
+
+			for v := range c {
+				chOut <- v
+				amounts[index]++
+			}
+		}(i, outs[i])
+	}
+
+
+	go func() {
+		wg.Wait()
+		close(chOut)
+	}()
+
+	var count int64 // количество чисел результирующего канала
+	var sum int64   // сумма чисел результирующего канала
+
+    for v := range chOut {
+		count++
+		sum += v 
+	}
+
+	fmt.Println("Количество чисел", inputCount, count)
+	fmt.Println("Сумма чисел", inputSum, sum)
+	fmt.Println("Разбивка по каналам", amounts)
+
+	// проверка результатов
+	if inputSum != sum {
+		log.Fatalf("Ошибка: суммы чисел не равны: %d != %d\n", inputSum, sum)
+	}
+	if inputCount != count {
+		log.Fatalf("Ошибка: количество чисел не равно: %d != %d\n", inputCount, count)
+	}
+	for _, v := range amounts {
+		inputCount -= v
+	}
+	if inputCount != 0 {
+		log.Fatalf("Ошибка: разделение чисел по каналам неверное\n")
+	}
+}
